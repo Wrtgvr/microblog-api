092144f7ff25883cf7739231f5c125345c21206f
Wrtgvr <saldaniil123@gmail.com>
2025-06-09 14:47:00 +0700

    Added: DB open funcs

---
+++ internal/repository/db.go
@@ -0,0 +1,24 @@
+package repository
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func MustOpenDb(dsn string) *gorm.DB {
+	db, err := OpenDb(dsn)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func OpenDb(dsn string) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("Database open error: %w", err)
+	}
+	return db, nil
+}
