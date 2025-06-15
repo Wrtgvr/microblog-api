19f3bd296b3fac7e178235db513f48a9707bf75d
Wrtgvr <saldaniil123@gmail.com>
2025-06-15 17:56:06 +0700

    Added: auth middleware

---
+++ internal/middleware/authmiddleware.go
@@ -0,0 +1,47 @@
+package middleware
+
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	errsuit "github.com/wrtgvr/errsuit/core"
+	"github.com/wrtgvr/microblog/internal/pkg/jwt"
+	"github.com/wrtgvr/microblog/internal/utils"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenPrefix := "Bearer "
+
+		errHandleFunc := utils.MakeErrHandlerFunc(c)
+
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, tokenPrefix) {
+			errHandleFunc(errsuit.NewUnauthorized("missing authorization token", nil, false))
+			return
+		}
+
+		tokenStr := strings.TrimPrefix(authHeader, tokenPrefix)
+		err := jwt.ParseToken(tokenStr)
+		if err != nil {
+			errHandleFunc(errsuit.NewUnauthorized("invalid token", err, false))
+			return
+		}
+
+		tokenType, err := jwt.GetTokenType(tokenStr)
+		if err != nil || tokenType != jwt.TypeAccess {
+			errHandleFunc(errsuit.NewUnauthorized("invalid token", err, false))
+			return
+		}
+
+		userId, err := jwt.GetUserIdFromToken(tokenStr)
+		if err != nil {
+			errHandleFunc(errsuit.NewUnauthorized("invalid token payload", err, true))
+			return
+		}
+
+		c.Set("user_id", userId)
+
+		c.Next()
+	}
+}
--- internal/router/router.go
+++ internal/router/router.go
@@ -4,7 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 	posthandler "github.com/wrtgvr/microblog/internal/handlers/post"
 	userhandler "github.com/wrtgvr/microblog/internal/handlers/user"
+	"github.com/wrtgvr/microblog/internal/middleware"
 )
+
+var authMw = middleware.AuthMiddleware()
 
 func NewRouter() *gin.Engine {
 	r := gin.New()
@@ -16,17 +19,20 @@ func NewRouter() *gin.Engine {
 }
 
 func RegisterUserRoutes(r *gin.Engine, h *userhandler.UserHandler) {
-	g := r.Group("users")
+	g := r.Group("/users")
 	{
-		g.GET("/me", func(ctx *gin.Context) {})       // user get their account info -USER ROLE
-		g.GET("/me/posts", func(ctx *gin.Context) {}) // get user's posts -USER ROLE
-		g.PATCH("/me", func(ctx *gin.Context) {})     // user updates their account info -USER ROLE
-		g.DELETE("/me", func(ctx *gin.Context) {})    // user delete their accout -USER ROLE
-
-		g.GET("/:id", func(ctx *gin.Context) {})       // get user account info with given id -GUEST ROLE
-		g.GET("/:id/posts", func(ctx *gin.Context) {}) // get user's posts -GUEST ROLE
-		g.PATCH("/:id", func(ctx *gin.Context) {})     // update account info with given id -ADMIN ROLE (if user is acc owner then -USER ROLE)
-		g.DELETE("/:id", func(ctx *gin.Context) {})    // delete account info with given id -ADMIN ROLE (if user is acc owner then -USER ROLE)
+		meGroup := g.Group("me")
+		meGroup.Use(authMw)
+		{
+			g.GET("", func(ctx *gin.Context) {})       // user get their account info -USER ROLE
+			g.GET("/posts", func(ctx *gin.Context) {}) // get user's posts -USER ROLE
+			g.PATCH("", func(ctx *gin.Context) {})     // user updates their account info -USER ROLE
+			g.DELETE("", func(ctx *gin.Context) {})    // user delete their accout -USER ROLE
+		}
+		g.GET("/:id", func(ctx *gin.Context) {})            // get user account info with given id -GUEST ROLE
+		g.GET("/:id/posts", func(ctx *gin.Context) {})      // get user's posts -GUEST ROLE
+		g.PATCH("/:id", authMw, func(ctx *gin.Context) {})  // update account info with given id -ADMIN ROLE (if user is acc owner then -USER ROLE)
+		g.DELETE("/:id", authMw, func(ctx *gin.Context) {}) // delete account info with given id -ADMIN ROLE (if user is acc owner then -USER ROLE)
 	}
 }
 
