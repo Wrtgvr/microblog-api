e07dc53271c24ae3ad38ba4633d95d29cb224ed1
Wrtgvr <saldaniil123@gmail.com>
2025-06-16 17:53:23 +0700

    Added: RefreshAuthMiddleware

--- internal/middleware/authmiddleware.go
+++ internal/middleware/authmiddleware.go
@@ -45,3 +45,48 @@ func AuthMiddleware() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+func RefreshAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenPrefix := "Bearer "
+
+		errHandleFunc := utils.MakeErrHandlerFunc(c)
+
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, tokenPrefix) {
+			errHandleFunc(errsuit.NewUnauthorized("missing authorization token", nil, false))
+			return
+		}
+
+		tokenStr := strings.TrimPrefix(authHeader, tokenPrefix)
+		err := jwt.ParseToken(tokenStr)
+		if err != nil {
+			errHandleFunc(errsuit.NewUnauthorized("invalid token", err, false))
+			return
+		}
+
+		tokenType, err := jwt.GetTokenType(tokenStr)
+		if err != nil || tokenType != jwt.TypeRefresh {
+			errHandleFunc(errsuit.NewUnauthorized("invalid token", err, false))
+			return
+		}
+
+		jti, err := jwt.GetJtiFromRefreshToken(tokenStr)
+		if err != nil {
+			errHandleFunc(errsuit.NewUnauthorized("invalid token payload", err, false))
+			return
+		}
+
+		c.Set("jti", jti)
+
+		userId, err := jwt.GetUserIdFromToken(tokenStr)
+		if err != nil {
+			errHandleFunc(errsuit.NewUnauthorized("invalid token payload", err, false))
+			return
+		}
+
+		c.Set("user_id", userId)
+
+		c.Next()
+	}
+}
