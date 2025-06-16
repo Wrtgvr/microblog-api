c663c8ece75c9664aa541287ad78aac930ffcc34
Wrtgvr <saldaniil123@gmail.com>
2025-06-16 17:59:46 +0700

    Added: RegisterAuthRoutes func. MWs on some routes

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,12 +2,14 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	authhandler "github.com/wrtgvr/microblog/internal/handlers/auth"
 	posthandler "github.com/wrtgvr/microblog/internal/handlers/post"
 	userhandler "github.com/wrtgvr/microblog/internal/handlers/user"
 	"github.com/wrtgvr/microblog/internal/middleware"
 )
 
 var authMw = middleware.AuthMiddleware()
+var resfreshAuthMw = middleware.RefreshAuthMiddleware()
 
 func NewRouter() *gin.Engine {
 	r := gin.New()
@@ -21,7 +23,7 @@ func NewRouter() *gin.Engine {
 func RegisterUserRoutes(r *gin.Engine, h *userhandler.UserHandler) {
 	g := r.Group("/users")
 	{
-		meGroup := g.Group("me")
+		meGroup := g.Group("/me")
 		meGroup.Use(authMw)
 		{
 			g.GET("", func(ctx *gin.Context) {})       // user get their account info -USER ROLE
@@ -37,11 +39,19 @@ func RegisterUserRoutes(r *gin.Engine, h *userhandler.UserHandler) {
 }
 
 func RegisterPostsRoutes(r *gin.Engine, h *posthandler.PostHandler) {
-	g := r.Group("posts")
+	g := r.Group("/posts")
 	{
-		g.GET("/:id", func(ctx *gin.Context) {})    // get post with given id -GUEST ROLE
-		g.DELETE("/:id", func(ctx *gin.Context) {}) // delete post with given id -ADMIN ROLE (if user is post author then -USER ROLE)
+		g.GET("/:id", func(ctx *gin.Context) {})            // get post with given id -GUEST ROLE
+		g.DELETE("/:id", authMw, func(ctx *gin.Context) {}) // delete post with given id -ADMIN ROLE (if user is post author then -USER ROLE)
 
-		g.POST("", func(ctx *gin.Context) {}) // create new post -USER ROLE
+		g.POST("", authMw, func(ctx *gin.Context) {}) // create new post -USER ROLE
+	}
+}
+
+func RegisterAuthRoutes(r *gin.Engine, h *authhandler.AuthHandler) {
+	g := r.Group("/auth")
+	{
+		g.POST("/register", resfreshAuthMw, func(ctx *gin.Context) {})
+		g.POST("/login", resfreshAuthMw, func(ctx *gin.Context) {})
 	}
 }
